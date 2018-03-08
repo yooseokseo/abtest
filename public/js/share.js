c1d7cd0f4a4eabf778198fc2c17908589f95aac5
mrpeem <31452811+mrpeem@users.noreply.github.com>
2018-03-08 10:38:21 -0800

    updated /js/share.js for email

--- public/js/share.js
+++ public/js/share.js
@@ -3,11 +3,23 @@
 $(document).ready(function() {
 
   $('#link-send').click(function(){
-    console.log('send');
-    $('.save-popup').html('<img id="check-icon" src="/images/icons/check-500.png" alt=""> Sent Link');
+    if (email == undefined)
+    {
+      $('.save-popup').html('<img id="check-icon" src="/images/icons/x-mark.png" alt=""> Enter email');
+    }
+    else
+    {
+      var email = $("#email").val();
+      var link = $("#link").val();
+      socket.emit('email', email, link);
+
+      console.log('send');
+      $('.save-popup').html('<img id="check-icon" src="/images/icons/check-500.png" alt=""> Sent Link');
+    }
     $('.save-popup').fadeIn(500);
     $('.save-popup').fadeOut(2000);
   })
+	
   $('#link-copy').click(function(){
     $('.save-popup').html('<img id="check-icon" src="/images/icons/check-500.png" alt=""> Copied Link');
     $('.save-popup').fadeIn(500);
