b6cdc76cbb05b5f5e0d16485516e19f0ce473f52
mrpeem <31452811+mrpeem@users.noreply.github.com>
2018-03-08 10:41:30 -0800

    updated js/share.js for email

--- public/js/share.js
+++ public/js/share.js
@@ -1,13 +1,29 @@
 'use strict';
+//var socket = io.connect('http://localhost:3000');
+var socket = io.connect('a8-team2-abtest.herokuapp.com/');
+
 
 $(document).ready(function() {
 
+
   $('#link-send').click(function(){
-    console.log('send');
-    $('.save-popup').html('<img id="check-icon" src="/images/icons/check-500.png" alt=""> Sent Link');
+    if (email == undefined)
+    {
+      $('.save-popup').html('<img id="check-icon" src="/images/icons/x-mark.png" alt=""> Enter email');
+    }
+    else
+    {
+      var email = $("#email").val();
+      var link = $("#link").val();
+      socket.emit('email', email, link);
+
+      console.log('send');
+      $('.save-popup').html('<img id="check-icon" src="/images/icons/check-500.png" alt=""> Sent Link');
+    }
     $('.save-popup').fadeIn(500);
     $('.save-popup').fadeOut(2000);
   })
+	
   $('#link-copy').click(function(){
     $('.save-popup').html('<img id="check-icon" src="/images/icons/check-500.png" alt=""> Copied Link');
     $('.save-popup').fadeIn(500);
